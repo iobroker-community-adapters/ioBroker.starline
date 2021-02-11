59ac787e8cec36f3e7948f613e4eea76377dcd58
instalator <vvvalt@mail.ru>
2021-02-11 09:25:10 +0700

    1.1.3

--- admin/words.js
+++ admin/words.js
@@ -64,17 +64,5 @@ systemDictionary = {
     "on save adapter restarts with new config immediately": {
         "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
         "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
-    },
-    "donate":                                      {
-        "en": "I am writing a code in Siberia, I drink a lot of hot tea and coffee. Thanks!",
-        "de": "Ich schreibe einen Code in Sibirien, ich trinke viel heißen Tee und Kaffee. Vielen Dank!",
-        "ru": "Пишу код в Сибири, пью много горячего чая и кофе. Спасибо!",
-        "pt": "Estou escrevendo um código na Sibéria, bebo muito chá quente e café. Obrigado!",
-        "nl": "Ik schrijf een code in Siberië, ik drink veel hete thee en koffie. Bedankt!",
-        "fr": "J'écris un code en Sibérie, je bois beaucoup de thé et de café chauds. Merci!",
-        "it": "Sto scrivendo un codice in Siberia, bevo molto tè e caffè caldi. Grazie!",
-        "es": "Estoy escribiendo un código en Siberia, bebo mucho té y café calientes. ¡Gracias!",
-        "pl": "Piszę kod na Syberii, piję dużo gorącej herbaty i kawy. Dzięki!",
-        "zh-cn": "我在西伯利亚写代码，我喝很多热茶和咖啡。谢谢！"
     }
 };
